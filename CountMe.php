cf6efa871cf0e3a9bc3b943e605b5a3ba6a80202
ipuxin <ipuxin@qq.com>
2016-10-18 16:48:59 +0800

    OuterImpl 重写:current(),key()方法 ar 修改数组内容

--- CountMe.php
+++ CountMe.php
@@ -1,15 +1,40 @@
 <?php
+date_default_timezone_set('PRC');
 
-class CountMe
+/**
+ * Class OuterImpl
+ * 重写:current(),key()方法
+ * 修改数组内容
+ */
+class OuterImpl extends IteratorIterator
 {
-    protected $_myConunt = 3;
-
-    public function count()
+    public function current()
     {
-        return $this->_myConunt;
+        return parent::current() . '_children';
+    }
+
+    public function key()
+    {
+        return parent::current() . '_children';
     }
 }
 
-$obj = new CountMe();
-//1
-echo count($obj);
+$people = [
+    'people1' => '张三',   //position = 0
+    'people2' => '李四', //position = 1
+    'people3' => '王二',
+    'people4' => '王二是',
+];
+
+$arrIterator = new ArrayIterator($people);
+$outObj = new OuterImpl($arrIterator);
+
+/**
+ * 张三_children : 张三_children
+ * 李四_children : 李四_children
+ * 王二_children : 王二_children
+ * 王二是_children : 王二是_children
+ */
+foreach ($outObj as $key => $value) {
+    echo $key . ' : ' . $value . "<br>";
+}
